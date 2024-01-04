db6982b90209c89cb52492f60244ef8e4b29b8fc
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-04 15:06:51 -0300

    finish exercise03

--- POO1.Exercises/Exercises/Exercise03.cs
+++ POO1.Exercises/Exercises/Exercise03.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POO1.Exercises.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,41 @@ namespace POO1.Exercises.Exercises
     {
         public override void Execute()
         {
-            throw new NotImplementedException();
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Informe um número inteiro positivo que deseja calcular seu fatorial: ");
+                    if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+                    {
+                        Console.WriteLine($"O fatorial de {number} é {Factorial(number)}.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Por favor, digite um número inteiro positivo maior que 0.");
+                        Menu.WaitInput();
+                    }
+                }
+                Menu.WaitInput();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static long Factorial(int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+
+            long result = 1;
+
+            for (int i = 2; i <= n; i++)
+                result *= i;
+
+            return result;
         }
     }
 }
