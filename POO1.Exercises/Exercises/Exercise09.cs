95072d1be1b36aa06e3edd16bd5d3c3298f107ce
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-04 15:58:14 -0300

    finish exercise 09

--- POO1.Exercises/Exercises/Exercise09.cs
+++ POO1.Exercises/Exercises/Exercise09.cs
@@ -10,7 +10,47 @@ namespace POO1.Exercises.Exercises
     {
         public override void Execute()
         {
-            throw new NotImplementedException();
+            try
+            {
+                Console.Write("Digite o CPF que deseja validar (apenas numeros): ");
+                string cpf = Console.ReadLine();
+
+                if (ValidateCPF(cpf))
+                    Console.WriteLine("CPF válido.");
+                else
+                    Console.WriteLine("CPF inválido.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static bool ValidateCPF(string cpf)
+        {
+            if (cpf.Length != 11 || !long.TryParse(cpf, out _))
+                return false;
+
+            int sum = 0;
+            for (int i = 0; i < 9; i++)
+                sum += int.Parse(cpf[i].ToString()) * (10 - i);
+            
+            int resto = sum % 11;
+            int firstCheckDigit = (resto < 2) ? 0 : 11 - resto;
+
+            if (int.Parse(cpf[9].ToString()) != firstCheckDigit)
+                return false;
+
+            sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += int.Parse(cpf[i].ToString()) * (11 - i);
+            }
+            resto = sum % 11;
+            int secondCheckDigit = (resto < 2) ? 0 : 11 - resto;
+
+            return int.Parse(cpf[10].ToString()) == secondCheckDigit;
         }
     }
 }
