d2338574c628dcdcf076a822d50a6b3ebed3f727
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-04 15:00:04 -0300

    finish exercise 2

--- POO1.Exercises/Exercises/Exercise02.cs
+++ POO1.Exercises/Exercises/Exercise02.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using POO1.Exercises.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,42 @@ namespace POO1.Exercises.Exercises
     {
         public override void Execute()
         {
-            throw new NotImplementedException();
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Informe um número inteiro positivo que deseja verificar se é primo ou não: ");
+                    if (int.TryParse(Console.ReadLine(), out int number) && number > 1)
+                    {
+                        if (IsPrime(number))
+                            Console.WriteLine($"{number} é um número primo.");
+                        else
+                            Console.WriteLine($"{number} não é um número primo.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Por favor, digite um número inteiro positivo maior que 1.");
+                        Menu.WaitInput();
+                    }
+                }
+                Menu.WaitInput();
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static bool IsPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
         }
     }
 }
