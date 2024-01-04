aec03e64e73b8aaa53d8b1f17a19b39a3a43a2d3
paulaandrezza <paulaandrezza25@gmail.com>
2024-01-04 15:47:11 -0300

    finish exercise08

--- POO1.Exercises/Exercises/Exercise08.cs
+++ POO1.Exercises/Exercises/Exercise08.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace POO1.Exercises.Exercises
@@ -10,7 +11,27 @@ namespace POO1.Exercises.Exercises
     {
         public override void Execute()
         {
-            throw new NotImplementedException();
+            try
+            {
+                Console.Write("Digite a senha que deseja validar: ");
+                string password = Console.ReadLine();
+
+                if (ValidatePassword(password))
+                    Console.WriteLine("Senha válida!");
+                else
+                    Console.WriteLine("Senha inválida. Certifique-se de que atende aos critérios.");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static bool ValidatePassword(string password)
+        {
+            Regex regexPassword = new Regex(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$");
+            return regexPassword.IsMatch(password);
         }
     }
 }
